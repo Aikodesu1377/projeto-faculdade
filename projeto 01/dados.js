c4794f20bb588f65d53d129fb3bb2ff2517430c1
Aikodesu1377 <lisaoliveira14042003@gmail.com>
2024-09-16 19:21:40 -0300

    melhora de lógica de botões e pequena pequena aprimoração de estruturas html

--- projeto 01/dados.js
+++ projeto 01/dados.js
@@ -1,70 +1,33 @@
 let dados = [
     {
-        classe: "ninja",
+        classe: "Ninja",
         vida: "100",
         ataqueCorpo: "200",
         ataqueDistancia: "0"
     },
     {
-        classe: "anjo-caido",
+        classe: "Anjo-Caido",
         vida: "150",
         ataqueCorpo: "20",
         ataqueDistancia: "200"
-    },
+    }, 
     {
-        classe: "guerreiro",
+        classe: "Guerreiro",
         vida: "200",
         ataqueCorpo: "150",
         ataqueDistancia: "0"
     },
     {
-        classe: "gato-arqueiro",
+        classe: "Gato-Arqueiro",
         vida: "50",
         ataqueCorpo: "10",
         ataqueDistancia: "300"
     },
     {
-        classe: "cervo-guerreiro",
+        classe: "Cervo-Guerreiro",
         vida: "600",
         ataqueCorpo: "50",
         ataqueDistancia: "0"
     }
 ]
-// Adiciona os dados aos personagens
-let dadosPersonagem = document.getElementById('dados-jogador')
-dadosPersonagem.innerHTML = 
-`
-<p>vida:${dados[0].vida}</p>
-<p>ataqueCorpo:${dados[0].ataqueCorpo}</p>
-<p>ataqueDistancia:${dados[0].ataqueDistancia}</p>
-`
-let dadosPersonagem1 = document.getElementById('dados-jogador1')
-dadosPersonagem1.innerHTML = 
-`
-<p>vida:${dados[1].vida}</p>
-<p>ataqueCorpo:${dados[1].ataqueCorpo}</p>
-<p>ataqueDistancia:${dados[1].ataqueDistancia}</p>
-`
-let dadosPersonagem2 = document.getElementById('dados-jogador2')
-dadosPersonagem2.innerHTML = 
-`
-<p>vida:${dados[2].vida}</p>
-<p>ataqueCorpo:${dados[2].ataqueCorpo}</p>
-<p>ataqueDistancia:${dados[2].ataqueDistancia}</p>
-`
-let dadosPersonagem3 = document.getElementById('dados-jogador3')
-dadosPersonagem3.innerHTML = 
-`
-<p>vida:${dados[3].vida}</p>
-<p>ataqueCorpo:${dados[3].ataqueCorpo}</p>
-<p>ataqueDistancia:${dados[3].ataqueDistancia}</p>
-`
-let dadosPersonagem4 = document.getElementById('dados-jogador4')
-dadosPersonagem4.innerHTML = 
-`
-<p>vida:${dados[4].vida}</p>
-<p>ataqueCorpo:${dados[4].ataqueCorpo}</p>
-<p>ataqueDistancia:${dados[4].ataqueDistancia}</p>
-`
-
 
--- projeto 01/logica.js
+++ projeto 01/logica.js
@@ -1,15 +1,26 @@
-
 let classes = document.getElementById("define-classe");
 for (let i = 0; i < dados.length; i++) {
   const dado = dados[i];
   const button = document.createElement("button");
-  button.textContent = dado.classe;
 
-  // Adiciona o evento onclick ao botão, ajustando o caminho para o arquivo
+  // Cria um elemento <p> para os detalhes
+  const details = document.createElement("p");
+  details.innerHTML = `
+    <strong>Vida:</strong> ${dado.vida}<br>
+    <strong>Ataque corpo a corpo:</strong> ${dado.ataqueCorpo}<br>
+    <strong>Magia:</strong> ${dado.ataqueDistancia}
+  `;
+
+  // Adiciona o texto da classe ao botão e o elemento <p> como filho
+  button.textContent = dado.classe;
+  button.appendChild(details);
+
+  // Adiciona o evento onclick ao botão
   button.onclick = function() {
-    window.location.href = `classe${i}.html`; // Assumindo que os arquivos estão na mesma pasta
+  window.location.href = `classe${i}.html`;
   };
 
   classes.appendChild(button);
 }
 
+// ####proxima update sistema de vida dos personagens####
